aec3c93de197b3f454a26e573a66a73ac169e162
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-07-11 10:16:19 +0600

    hello buetpx

--- command_react.md
+++ command_react.md
@@ -4,4 +4,10 @@
 npx create-react-app react_buetpx
 cd react_buetpx
 npm start
+npm install react-router-dom@6
+```
+
+* if react er bhitorer git folder delete 
+```
+rmdir /S .git
 ```
--- frontend/react_buetpx/src/App.js
+++ frontend/react_buetpx/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1> hello buetpx </h1>
+        
+      </div>
+      {/* this route because router-react-dom dont work link without this lines */}
+      <Routes>
+        <Route exact path="/" />
+      </Routes>
+    </Router>
   );
 }
 
