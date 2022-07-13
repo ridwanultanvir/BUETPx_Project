1eca39eab35595da1c26ecfebb8ec17608f5a6b8
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-07-13 15:05:45 +0600

    gitignore

--- frontend/react_buetpx/src/App.js
+++ frontend/react_buetpx/src/App.js
@@ -19,24 +19,3 @@ function App() {
 }
 
 export default App;
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import NavigationBar from "./sections/NavigationBar";
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        {/* <h1> hello buetpx </h1> */}
-        <NavigationBar />
-        
-      </div>
-      {/* this route because router-react-dom dont work link without this lines */}
-      <Routes>
-        <Route exact path="/" />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
