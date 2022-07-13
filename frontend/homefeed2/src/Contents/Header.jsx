f65d84724bc27072172d10b4f4d873f4634c05ec
Naeem Ahmed <1705014@ugrad.cse.buet.ac.bd>
2022-07-14 05:00:26 +0600

    header update

--- frontend/homefeed2/src/Contents/Header.jsx
+++ frontend/homefeed2/src/Contents/Header.jsx
@@ -1,38 +1,8 @@
-import React from 'react'
-import {AppBar,Toolbar,Typography} from '@mui/material'
-import AcUnitRoundedIcon from "@mui/icons-material/AcUnitRounded";
-import { makeStyles } from "@mui/styles";
-import Button from '@mui/material/Button';
-
-const useStyles = makeStyles(() => ({
-  typographyStyles: {
-    flex: 1
-  }
-}));
-
-function Header() {
-  const classes = useStyles();
-  return (
-    <AppBar position='static'>
-        <Toolbar>
-            <Typography className={classes.typographyStyles}>
-                BUETpx
-            </Typography>
-            {/* <Button size="small"><AcUnitRoundedIcon/></Button> */}
-            <AcUnitRoundedIcon/>
-            
-        </Toolbar>
-        
-    </AppBar>
-  )
-}
-
-export default Header
-
-// import React from "react";
-// import { AppBar, Toolbar, Typography } from "@material-ui/core";
-// import AcUnitRoundedIcon from "@material-ui/icons/AcUnitRounded";
-// import { makeStyles } from "@material-ui/styles";
+// import React from 'react'
+// import {AppBar,IconButton,Toolbar,Typography} from '@mui/material'
+// import AcUnitRoundedIcon from "@mui/icons-material/AcUnitRounded";
+// import { makeStyles } from "@mui/styles";
+// import Button from '@mui/material/Button';
 
 // const useStyles = makeStyles(() => ({
 //   typographyStyles: {
@@ -40,18 +10,195 @@ export default Header
 //   }
 // }));
 
-// const Header = () => {
+// function Header() {
 //   const classes = useStyles();
 //   return (
-//     <AppBar position="static">
-//       <Toolbar>
-//         <Typography className={classes.typographyStyles}>
-//           Anthony sistilli
-//         </Typography>
-//         <AcUnitRoundedIcon />
-//       </Toolbar>
-//     </AppBar>
-//   );
-// };
+//     <AppBar position='static'>
+//         <Toolbar>
+//             <Typography className={classes.typographyStyles}>
+//                 BUETpx
+//             </Typography>
+//             <IconButton ><AcUnitRoundedIcon /></IconButton>
+//         </Toolbar>
 
-// export default Header;
+//     </AppBar>
+//   )
+// }
+
+// export default Header
+
+// // npm install @mui/material @emotion/react @emotion/styled
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import CameraOutlinedIcon from '@mui/icons-material/CameraOutlined';
+
+const pages = ['Discover', 'Quest'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Header () {
+  // const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  // const handleOpenNavMenu = (event) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
+  // const handleOpenUserMenu = (event) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
+
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
+
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <CameraOutlinedIcon 
+            sx={{ 
+              display: { xs: 'none', md: 'flex' }, mr: 1 
+              }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'revert-layer',
+              fontWeight: 700,
+              // letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            BUETpx
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              // onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              // anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              // open={Boolean(anchorElNav)}
+              // onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} 
+                // onClick={handleCloseNavMenu}
+                >
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography> */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                // onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton 
+              // onClick={handleOpenUserMenu} 
+              sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              // anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              // open={Boolean(anchorElUser)}
+              // onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} 
+                // onClick={handleCloseUserMenu}
+                >
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
+export default Header;
