b313a46fbfc2fecd06bf22dd59764876772042f3
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-07-15 01:13:49 +0600

    category showing

--- frontend/buetpx_frontend/src/Post/Post.js
+++ frontend/buetpx_frontend/src/Post/Post.js
@@ -11,26 +11,23 @@ import { Box } from '@mui/material';
 import Content from './post_content';
 import post_info from "./post_info";
 import Button from '@mui/material/Button';
-const sampleJSON = {
-    id: 2005,
-    post_title: "Sunny Day",
-    post_date: "2010-08-02T21:27:44+0000",
-    photo_url: "https://media.gettyimages.com/photos/central-business-district-of-sydney-at-daytime-picture-id973115274?k=20&m=973115274&s=612x612&w=0&h=X1ceWUm3p-TjYpz76w8HGeq9Azqwk6cgwKOLJODjFHQ=",
-    owner: 1004,
-    category: "landscape",
-    place: "Boston, MA",
-    tags: ["sun", "cloud", "sky"]
-};
-// const sampleJSON1 = {
-//     "id": 2005,
-//     "category": "landscape"
-//   };
+// const sampleJSON = {
+//     id: 2005,
+//     post_title: "Sunny Day",
+//     post_date: "2010-08-02T21:27:44+0000",
+//     photo_url: "https://media.gettyimages.com/photos/central-business-district-of-sydney-at-daytime-picture-id973115274?k=20&m=973115274&s=612x612&w=0&h=X1ceWUm3p-TjYpz76w8HGeq9Azqwk6cgwKOLJODjFHQ=",
+//     owner: 1004,
+//     category: "landscape",
+//     place: "Boston, MA",
+//     tags: ["sun", "cloud", "sky"]
+// };
+
 function Post(props) {
     const getTag = tag => {
         return (
-        //   <Grid item xs={12} sm={4}>
-            <Button> {tag} </Button>
-        //   </Grid>
+          // <Grid item xs={2} sm={4}>
+        <Button variant="outlined" color="secondary" >   {tag} </Button>
+          //  </Grid>
         );
       };
   return (
@@ -62,16 +59,19 @@ function Post(props) {
           </Grid>
 
           <Grid item container xs={6} direction='column' >
-            <Grid item >
+            <Grid item>
+            <Button variant="outlined" color="primary">   Keywords </Button>
             {/* <Button> Keywords:</Button> */}
-            Keywords : {post_info[0].tags.map(tag => getTag(tag))}
+            {post_info[0].tags.map(tag => getTag(tag))}
             
 
             </Grid>
+            {/* <Grid item> {post_info[0].tags.map(tag => getTag(tag))} </Grid> */}
             <Grid item >
             
             {/* <Content /> */}
-            Category : {sampleJSON.category}
+            <Button variant="outlined" color="primary">   Category </Button>
+            <Button variant="outlined" color="secondary">   {post_info[0].category} </Button>
             </Grid>
 
             
