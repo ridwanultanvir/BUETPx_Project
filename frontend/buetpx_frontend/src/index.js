ad00d75a5448a91cf69412671150d4aa4afd78eb
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-07-14 15:16:04 +0600

    about page added (just start)

--- frontend/buetpx_frontend/src/index.js
+++ frontend/buetpx_frontend/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import About from './About'; 
+// import {
+//   Navigation,
+//   Footer,
+//   Home,
+//   About,
+//   Contact,
+//   Blog,
+//   Posts,
+//   Post,
+// } from "./components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      
+    </Routes>
+    
+  </Router>,
+
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// serviceWorker.unregister();
