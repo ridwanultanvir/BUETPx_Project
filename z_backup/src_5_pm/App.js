cd1e1b33128520625837a44579231ff9d33f535f
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-07-14 11:04:28 +0600

    test2

--- z_backup/src_5_pm/App.js
+++ z_backup/src_5_pm/App.js
@@ -2,13 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import NavigationBar from "./sections/NavigationBar";
-import Album from "./sections/Album";
 function App() {
   return (
     <Router>
       <div className="App">
         {/* <h1> hello buetpx </h1> */}
-        <Album />
+        <NavigationBar />
        
       </div>
       {/* this route because router-react-dom dont work link without this lines */}
