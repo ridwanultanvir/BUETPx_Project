229140edb1a8c309c7a46b45961a2d7e194e2870
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-07-16 21:03:50 +0600

    post redirection showing ok

---
+++ frontend/buetpx_frontend/src/Post/post_single.js
@@ -0,0 +1,12 @@
+export default 
+{
+  id: 2005,
+  post_title: "Sunny Day",
+  post_date: "2010-08-02T21:27:44+0000",
+  photo_url: "https://media.gettyimages.com/photos/central-business-district-of-sydney-at-daytime-picture-id973115274?k=20&m=973115274&s=612x612&w=0&h=X1ceWUm3p-TjYpz76w8HGeq9Azqwk6cgwKOLJODjFHQ=",
+  owner: 1004,
+  category: "travel",
+  place: "Boston, MA",
+  tags: ["boat", "beach", "sky"]
+}
+    
