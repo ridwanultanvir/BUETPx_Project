0f1559bd6e10d0a977197de373e27331310b6001
ridwanultanvir <1705016@ugrad.cse.buet.ac.bd>
2022-08-10 22:49:14 +0600

    header showing

--- frontend/buetpx_frontend/src/Component/ImageList/ImageList1.js
+++ frontend/buetpx_frontend/src/Component/ImageList/ImageList1.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import {Grid} from "@mui/material";
-// import Header from './Contents/Header';
-import Header from '../../Static/Header';
-import Body from '../../Static/Body';
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+function srcset(image: string, size: number, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+export default function QuiltedImageList() {
+  return (
+    <ImageList
+      sx={{ width: 500, height: 450 }}
+      variant="quilted"
+      cols={4}
+      rowHeight={121}
+    >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <img
+            {...srcset(item.img, 121, item.rows, item.cols)}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
 
 const itemData = [
   {
@@ -64,44 +92,3 @@ const itemData = [
     cols: 2,
   },
 ];
-
-
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
-
-function ImageList() {
-  return (
-   
-      <Grid container direction='column'>
-        <Grid item>
-        <Header/>
-        </Grid>
-        
-
-        <Grid item container>
-          <Grid item xs={0} sm={1}>
-
-          </Grid>
-
-          <Grid item xs={12} sm={10} >
-          <h1> Ami Tanvir</h1>
-          </Grid>
-
-          <Grid item xs={0} sm={1} >
-
-          </Grid>
-         
-
-        </Grid>
-      </Grid>
-  
-  );
-}
-
-export default ImageList;
