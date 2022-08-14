b88a75d81ab42f501abb1cdf80ac825beb90c5e4
Naeem Ahmed <1705014@ugrad.cse.buet.ac.bd>
2022-08-14 18:09:33 +0600

    homefeed prev

--- frontend/buetpx_frontend/src/Component/Homefeed/Homefeed.js
+++ frontend/buetpx_frontend/src/Component/Homefeed/Homefeed.js
@@ -1,46 +1,36 @@
 import React from 'react';
-import { Grid} from "@mui/material";
+import {Grid} from "@mui/material";
+// import Header from './Contents/Header';
 import Header from '../../Static/Header';
 import Body from '../../Static/Body';
 
-function Homefeed(props) {
+function Homefeed() {
   return (
    
       <Grid container direction='column'>
-      <Grid item>
-      <Header/>
-      </Grid>
-
-
-      <Grid item container>
-        <Grid item xs={0} sm={1}>
-
+        <Grid item>
+        <Header/>
+        </Grid>
         
 
-        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={1}>
 
-        <Grid item xs={12} sm={10} >
-                
-        <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-          <ImageList variant="masonry" cols={3} gap={20}>
-            {posts.map((post) => (
-              <ImgPost {...post}/>
-                  
-            ))}
-          </ImageList>
-        </Box>
-        </Grid>
+          </Grid>
 
-        <Grid item xs={0} sm={1} >
+          <Grid item xs={12} sm={10} >
+          <Body/>
+          </Grid>
 
-        </Grid>
-      
+          <Grid item xs={0} sm={1} >
+
+          </Grid>
+         
 
         </Grid>
       </Grid>
-                        
-    
-    );
-    }
+  
+  );
+}
 
-    export default Homefeed;
+export default Homefeed;
